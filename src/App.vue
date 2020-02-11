b11c3b36366788a4ee6b50de7a9a90556cfd4555
Daniel Crain <bluevision97@gmail.com>
2020-02-11 11:23:18 -0500

    whatever changes I made

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,28 @@
     <v-app-bar app color="primary" dark>
       <v-toolbar-title>CRM Application</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn text rounded to="/">Home</v-btn
-      ><v-btn text rounded to="/login">Login</v-btn>
+      <v-btn
+        v-for="link in links"
+        :key="`${link.label}-header-link`"
+        text
+        rounded
+        to="link.url"
+        >{{ link.label }}</v-btn
+      >
     </v-app-bar>
     <v-content><router-view></router-view></v-content>
     <v-footer color="primary lighten-1" padless>
       <v-row justify="center" no-gutters>
         <v-btn
           v-for="link in links"
-          :key="link"
+          :key="`${link.label}-footer-link`"
           color="white"
           text
           rounded
           class="my-2"
+          :to="link.url"
         >
-          {{ link }}
+          {{ link.label }}
         </v-btn>
         <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
           {{ new Date().getFullYear() }} — <strong>CRM Application</strong>
@@ -34,8 +41,16 @@ export default {
   components: {},
 
   data: () => ({
-    showPassword: false,
-    links: ["Home", "Login"]
+    links: [
+      {
+        label: "Home",
+        url: "/"
+      },
+      {
+        label: "Login",
+        url: "/login"
+      }
+    ]
   })
 };
 </script>
