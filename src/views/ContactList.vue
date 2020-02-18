eedef9f7d4f9a8bbe59b830f61a3751443655c10
Administrator <Administrator@imperialcrs.com>
2020-02-17 22:39:49 -0500

    Added contacts list

--- src/views/ContactList.vue
+++ src/views/ContactList.vue
@@ -1,29 +1,40 @@
 <template>
-<v-container>
-  <v-row>
-    <v-col v-for="category in categories" :key="category.title">
-      <h1>{{category.title}}</h1>
-    </v-col>
-  </v-row>
-
-</v-container>
-
-  
+  <v-card
+    class="mx-auto"
+    max-width="100%"
+    tile
+  >
+    <v-list>
+      <v-subheader>
+        <v-text-field>
+        <v-icon slot="append">mdi-magnify</v-icon>
+      <v-icon slot="prepend" @click="addContact">mdi-plus-circle</v-icon>
+      </v-text-field>
+      </v-subheader>
+      <v-list-item-group v-model="item" color="primary">
+        <v-list-item
+          v-for="(contact, i) in contacts"
+          :key="i"
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="contact.firstName"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-card>
 </template>
 
 <script>
-import ToDo from '../components/ToDo'
 import contacts from './contacts.json'
 export default {
 data() {
-  return {
-    computed: {
-      categories() {
-        return this.$store.state.categories
-      }
-    },
-    contacts: contacts
+      return {contacts: contacts}
+  },
+methods: {
+  addContact() {
+    alert('Hello')
   }
-},
+}
 }
 </script>
