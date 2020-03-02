6da9fd634fa15b40750b056fc3f203142c8d9313
Administrator <Administrator@imperialcrs.com>
2020-03-02 12:25:29 -0500

    Adding state

--- src/store.js
+++ src/store.js
@@ -5,6 +5,10 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
+        contact: {
+            id: '1',
+            name: 'Jar Jar Binks'
+        }
     },
     mutations: {
     },
